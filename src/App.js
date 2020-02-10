251f2a6eca0ebc4c61a686a478ebc7f8554c0a66
Nieminen Jani EXT <jani.nieminen@digia.com>
2020-02-10 07:55:41 +0200

    Changed initial commit.

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Testing ReactJS App in AWS Amplify!
         </p>
         <a
           className="App-link"
