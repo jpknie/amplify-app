5bcc6ccd2fbbd2c85169d6f4559026b1672d722e
Jani Nieminen <jpkniem@gmail.com>
2020-02-10 08:46:00 +0200

    Testing React Side Nav

--- src/App.js
+++ src/App.js
@@ -2,22 +2,31 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import SideNav, { Toggle, Nav, NavItem, NavIcon, NavText } from '@trendmicro/react-sidenav';
+import '@trendmicro/react-sidenav/dist/react-sidenav.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+import '@fortawesome/react-fontawesome';
+
 function App() {
   return (
     <div className="App">
+      <SideNav
+        onSelect={(selected) => console.log(selected)} style={{'background-color': '#00fa'}}>
+        <SideNav.Toggle />
+        <SideNav.Nav defaultSelected="home">
+          <NavItem eventKey="home">
+            <NavIcon>
+            <FontAwesomeIcon icon={faCoffee} />
+            </NavIcon>
+            <NavText>
+              Home
+            </NavText>
+          </NavItem>
+        </SideNav.Nav>
+      </SideNav>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Testing ReactJS App in AWS Amplify!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <p>Testing React side navigator</p>
       </header>
     </div>
   );
