41ae5407046f39be14922404930dfdc736fe2b07
Jani Nieminen <jpkniem@gmail.com>
2020-02-12 19:26:20 +0200

    View and format date.

--- src/RecordingView.js
+++ src/RecordingView.js
@@ -5,8 +5,17 @@ import Recording from './Recording';
 import './RecordingView.css';
 
 export default class RecordingView extends React.Component {
+    state = {
+        date: new Date(),
+    }
+
     constructor(props) {
         super(props);
+        this.dayChanged = this.onDayChange.bind(this);
+    }
+
+    onDayChange(date) {
+        this.setState({ date })
     }
 
     createRecordings() {
@@ -19,12 +28,26 @@ export default class RecordingView extends React.Component {
         return recordings;
     }
 
+    formatDate() {
+        let options = { 
+            weekday: 'long', 
+            year: 'numeric', 
+            month: 'long', 
+            day: 'numeric' 
+        };
+
+        return this.state.date.toLocaleDateString('fi-FI', options);
+    }
+
     render() {
         return(
             <div className="RecordingView">
                 <div className="CalendarFilter">
                     <span className="CalendarHeader">Filter by date</span>
-                    <Calendar />
+                    <Calendar onClickDay={this.dayChanged} />
+                </div>
+                <div className="DateHeader">
+                    <span>{this.formatDate()}</span>
                 </div>
                 <div className="RecordingsContainer">
                     {this.createRecordings()}
